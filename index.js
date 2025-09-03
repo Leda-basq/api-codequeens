cd0e7871bd21835ca408c7d399fe87ada17fe2df
DESKTOP-0CDRRID\Administrador <suporte@univali.br>
2025-09-03 16:50:34 -0300

    endpoint POST e GET do array pessoas

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-const express = require('express');
-const { monitorEventLoopDelay } = require('perf_hooks');
+const express = require("express");
+const { get } = require("http");
 const app = express();
 
 app.use(express.json());
 
 let pessoas = [
-    {nome:'leda', idade:13, cor:'rosa'},
-    {nome:'valentina', idade:13, cor:'roxo'},
-    {nome:'ana luiza', idade:13, cor:'rosa'},
-    {nome:'filipe', idade:16, cor:'preto'},
-    {nome:'agatha', idade:13, cor:'vermelho'},
-]
+  { id: 1, nome: "leda", idade: 13, cor: "rosa" },
+  { id: 2, nome: "valentina", idade: 13, cor: "roxo" },
+  { id: 3, nome: "ana luiza", idade: 13, cor: "rosa" },
+  { id: 4, nome: "filipe", idade: 16, cor: "preto" },
+  { id: 5, nome: "agatha", idade: 13, cor: "vermelho" },
+];
 
-app.get('/', (reg, res) =>{
-     res.json({ mensagem: 'API de pessoas funcionando'});
+app.get("/", (reg, res) => {
+  res.json({ mensagem: "API de pessoas funcionando" });
 });
 
-app.get('/pessoas', (reg, res) =>{
-    res.json(pessoas)
+app.get("/pessoas", (reg, res) => {
+  res.json(pessoas);
 });
 
-app.get('/pessoas2', (reg, res) =>{
-    res.json(pessoas[3])
+app.get("/pessoas2", (reg, res) => {
+  res.json(pessoas[3]);
+});
+
+app.post("/pessoas", (req, res) => {
+  const { nome, idade, cor } = req.body;
+  const newUser = { id: pessoas.length + 1, nome, idade, cor };
+  console.log(pessoas.length + 1);
+  console.log("Novos dados: ", newUser);
+  pessoas.push(newUser);
+  res.status(201).json(newUser); // 201 = código de criação com sucesso
+});
+
+//rota: http://localhost:3000/pessoas/2
+app.get("/pessoa/:id", (req,res) =>{
+    const id = parseInt(req.params.id);
+    const pessoa = pessoas.find(u => u.id === id);
+
+    if(!pessoa) {
+        return res.status(404).json({ error: "Usuário não encontrado"});
+    }
+
+    res.json(pessoa);
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-     console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
